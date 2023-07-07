9e63752bb1e3521158a3eddd32641124f54e0432
tbvjchvkfl <anfmdehdnjs@gmail.com>
2023-07-08 01:02:35 +0900

    Practice_Stack&Queue

---
+++ Queue/main.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+
+
+
+// QUEUE  (FIFO) ( First In First Out - 임의의 구조체 안에 먼저 들어간 정보가 먼저 빠져나옴 ) 
+
+enum command
+{
+	// 큐에 값을 집어 넣는다는 뜻
+	ENQUEUE = 1,
+	//큐에서 값을 뺀다는 뜻
+	DEQUEUE = 2,
+	Quit = 3
+};
+
+// 큐의 크기를 정함.
+const int QUEUE_SIZE{ 10 };
+
+struct Queue
+{
+	// 스택과 마찬가지로 값을 넣고 뺄 배열을 선언
+	int QArray[QUEUE_SIZE]{};
+
+	// 큐는 파이프 처럼 양쪽이 뚫려있는 형태이므로 값이 들어오고 빠져나가는 시작 점과 끝점으로 사용할 변수를 선언
+	// 스택과 비교하자면 스택에서는 값이 들어오고 나가는 위치로 사용했던 변수가 하나인거고
+	// 큐에서는 값이 들어오는 변수와 값이 빠져나가는 변수가 있는 형태
+	int head{};
+	int tail{};
+};
+
+void UserInteract(Queue& queue);
+void Enqueue(Queue& queue, int value);
+void Dequeue(Queue& queue);
+void QueueMessage(Queue& queue);
+
+
+int main()
+{
+	std::cout << "Circlear QUEUE" << std::endl;
+
+	Queue DataStructure;
+
+	UserInteract(DataStructure);
+
+	int command{};
+
+	while (true)
+	{
+		QueueMessage(DataStructure);
+		std::cout << ">";
+		std::cin >> command;
+
+		switch (command)
+		{
+		case ENQUEUE:
+		{
+			int value;
+			std::cout << "\tvalue : ";
+			std::cin >> value;
+			Enqueue(DataStructure, value);
+		}
+		break;
+		case DEQUEUE:
+		{
+			Dequeue(DataStructure);
+		}
+		break;
+		case Quit:
+		{
+			return 0;
+		}
+		break;
+
+		default:
+			std::cout << "잘못된 명령입니다." << std::endl;
+			break;
+		}
+	}
+}
+
+void UserInteract(Queue& queue)
+{
+	std::cout << "Queue" << std::endl;
+	std::cout << "1. Enqueue" << std::endl;
+	std::cout << "2. Dequeue" << std:: endl;
+	std::cout << "3. Quit" << std::endl;
+	std::cout << "-------------------------------" << std::endl;
+}
+
+
+
+void Enqueue(Queue& queue, int value)
+{
+
+	// tail의 값이 head의 값과 동일하다면 리턴
+	if ((queue.tail + 1) % QUEUE_SIZE == queue.head)
+	{
+		std::cout << "QUEUE is FULL!" << std::endl;
+		return;
+	}
+
+	// 값이 들어올 배열의 인덱스 즉, 임의의 구조체에 값을 넣을 때 사용할 포커스의 위치를 정해주는 것.
+	// 기본값이 0인 tail에 1을 더한 후 큐 사이즈로 나눈 나머지 값을 지정해준다.
+	// 현재 코드에서 Enqueue(Push)를 지속적으로 해주다가 tail의 값이 10이 되어 head의 값과 같아지면 큐가 가득찼다고 표시해주는 것.
+	// 즉, 배열의 0번 인덱스는 값이 들어가지 않음.
+	// 강사님께서 처음과 끝이 붙어있는 형태로 코드를 설명해주셔서 이렇게 짜여진 코드가 나온 것이지만,
+	// 어차피 큐의 크기를 정해주었다면 시작점과 끝점을 명시하는 방법으로 해도 될 것 같음. 이따가 해볼 것.
+	queue.tail = (queue.tail + 1) % QUEUE_SIZE;
+	 
+	// tail과 같은 값의 인덱스에 내가 입력한 값을 넣음.
+	queue.QArray[queue.tail] = value;
+}
+
+void Dequeue(Queue& queue)
+{
+	if (queue.head == queue.tail)
+	{
+		std::cout << "QUEUE is already EMPTY" << std::endl;
+	}
+
+	// 해당 코드에서는 시작 점과 끝점이 동일하다는 전제가 있었으므로
+	// Push했을 때와 마찬가지로
+	// Pop의 경우에도 함수가 실행될 때 head의 위치를 옮겨준다.
+	queue.head = (queue.head + 1) % QUEUE_SIZE;
+}
+
+void QueueMessage(Queue& queue)
+{
+	int i = queue.head;
+	if (queue.head == queue.tail)
+	{
+		std::cout << "Empty" << std::endl;
+	}
+
+	// head와 tail이 같아질 때까지 반복하며 tail의 인덱스에 있는 값을 출력해준다.
+	while (i != queue.tail)
+	{
+		i = (i + 1) % QUEUE_SIZE;
+		std::cout << queue.QArray[i] << std::endl;
+	}
+
+	std::cout << "-----------" << std::endl;
+}
--- Stack/Main.cpp
+++ Stack/Main.cpp
@@ -9,6 +9,7 @@ const int STACK_SIZE{ 10 };
 
 
 // 커맨드로 사용할 데이터들
+
 enum Command 
 {
 	PUSH = 1,
@@ -19,12 +20,14 @@ enum Command
 struct Stack
 {
 	// 스택으로 사용할 구조체는 실제 사용자와 상호작용하는 배열과 그와 관련된 정보들을 담고있어야한다.
-	// 
 	// 임의의 정보를 넣었다 뺐다 할 수 있도록 배열 (  배열의 크기는 위 상수타입의 변수로 사용 )
 	int Array[STACK_SIZE]{};
 
 
 	// 위 배열에 정보가 들어가 있지 않았을 때를 구분하기 위해 기본값이 안전 장치로 사용할 변수
+	// 해당 변수는 위 Array라는 배열의 인덱스로 사용될 변수임.
+	// 배열에 아무것도 들어가있지 않았다라는 것을 표현하기 위해 -1로 설정한 것.
+	// UI에서 포커스같은 용도
 	int topIndex{ -1 };
 };
 
@@ -80,9 +83,6 @@ int main()
 	return 0;
 }
 
-
-// 사용자와 직접적으로 상호작용할 요소들에 대한 내용
-// 위에 Command 
 void UserInteract(Stack& stack)
 {
 	std::cout << "STACK" << std::endl;
@@ -94,22 +94,34 @@ void UserInteract(Stack& stack)
 
 void Push(Stack& stack, int value)
 {
+
+	// 만약 배열의 모든 인덱스에 값이 들어와 있는 상태라면 리턴
+	// 배열의 0번 인덱스부터 값이 채워지는데, STACK_SIZE의 값은 10이었다. topIndex는 -1부터 값을 더해가며 증가하므로 STACK_SIZE에서 -1을 해준 것 
+	// 값을 10개 까지 넣을 껀데 배열의 10번 인덱스까지 사용하면 11개를 사용하는거니까 0~9번 인덱스까지 값을 넣을 수 있는데, 10번 인덱스에는 값을 넣을 수 없음.
 	if (stack.topIndex >= STACK_SIZE - 1)
 	{
 		std::cout << "Stack is FULL!!!" << std::endl;
 		return;
 	}
-	
+
+	// 해당 함수가 실행되면 위 Stack에 들어있던 배열(Array에 값을 집어 넣음 )
+	// 주의 사항!!!
+	// 배열의 인덱스에 topIndex를 넣었는데, topIndex의 기본값은 -1
+	// 배열의 인덱스는 0부터 시작하기 때문에 topIndex에 값을 더하여 0으로 만들어준 후에 입력한 값(매개변수)를 넣어줌. => stack.topIndex에 +1이 되면 0이 되므로 0번 인덱스부터 시작함.
 	stack.Array[++stack.topIndex] = value;
 }
 
 void Pop(Stack& stack)
 {
+	
 	if (stack.topIndex < 0)
 	{
 		std::cout << "Stack is empty!" << std::endl;
 		return;
 	}
+	// topIndex의 개수를 줄여줌
+	// 좀 더 쉽게 이해하면 topIndex는 임의의 UI에 포커스가 들어와있는 상태라고 이해하면 될 듯.
+	// 그 포커스를 하나씩 감소해주는 것.
 	stack.topIndex--;
 }
 
@@ -121,6 +133,8 @@ void StackMessage(Stack& stack)
 		std::cout << "Empty" << std::endl;
 		return;
 	}
+	// 임의의 변수에 topIndex 넣어 주고 반복하며 배열의 인덱스들을 출력
+	// i 는 하나씩 감소해야함. => Push했을 경우 topIndex 는 지속적으로 증가함. 배열의 특정 인덱스( i 값 )부터 0번 인덱스까지 들어가있는 값을 출력해야되기 때문
 	for (int i = stack.topIndex; i >= 0; i--)
 	{
 		std::cout << stack.Array[i] << std::endl;
